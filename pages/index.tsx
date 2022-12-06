91c31f6d0ed05fbedc8958fc7157f1bca17978d8
Chris <ghost@drednaut.net>
2022-12-06 09:44:36 -0800

    added email field
    Signed-off-by: Chris <ghost@drednaut.net>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,51 @@
-export default function Home({ res }: any) {
+import { useState } from 'react';
+import dashify from 'dashify';
+import axios from 'axios';
+
+const Post = () => {
+	const [content, setContent] = useState({
+		user: undefined,
+		email: undefined,
+		title: undefined,
+		body: undefined,
+	});
+	const [submitted, setSubmitted] = useState(false);
+	const onChange = (e: any) => {
+		const { value, name } = e.target;
+		setContent((prevState) => ({ ...prevState, [name]: value }));
+	};
+	const onSubmit = async () => {
+		const { user, title, body }: any = content;
+		await axios.post('/api/entry', { user, title, slug: dashify(title), body });
+		setSubmitted(true);
+	};
 	return (
 		<div className="container">
-			<h1>Home</h1>
-			<form>
-				<FormInput type="text" placeholder="Look at me!" label="First Thing" />
-				<FormInput type="text" placeholder="Look at me!" label="Second Thing" />
-				<FormInput type="text" placeholder="Look at me!" label="Third Thing" />
-				<FormInput type="text" placeholder="Look at me!" label="Fourth Thing" />
-			</form>
-			{res.response}
-		</div>
-	);
-}
-
-function FormInput(props: any) {
-	return (
-		<div>
-			{props.label}
+			<label htmlFor="title">Name</label>
+			<input type="text" name="user" value={content.user} onChange={onChange} />
+			<label htmlFor="title">Email</label>
 			<input
-				type={props.type}
-				className="form-input"
-				placeholder={props.placeholder}
+				type="text"
+				name="email"
+				value={content.email}
+				onChange={onChange}
 			/>
+			<label htmlFor="title">Title</label>
+			<input
+				type="text"
+				name="title"
+				value={content.title}
+				onChange={onChange}
+			/>
+			<label htmlFor="body">Body</label>
+			<textarea name="body" value={content.body} onChange={onChange} />
+			{!submitted ? (
+				<button onClick={onSubmit}>POST</button>
+			) : (
+				<button>Thanks!</button>
+			)}
 		</div>
 	);
-}
+};
 
-export async function getStaticProps() {
-	const data = await fetch('http://localhost:3000/api/hello');
-	const json = await data.json();
-
-	return {
-		props: {
-			res: json,
-		},
-	};
-}
+export default Post;
