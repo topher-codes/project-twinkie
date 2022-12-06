d86915abd90f83a2adb7e70715c32f55203fd48e
Atlessc <107712050+Atlessc@users.noreply.github.com>
2022-12-06 11:32:44 -0800

    new branch to push new edits

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,21 @@
 import { useState } from 'react';
 import dashify from 'dashify';
 import axios from 'axios';
+<<<<<<< HEAD
 import { ThankYou } from './thankyou';
+=======
+import { Rating } from '@mui/material';
+import { SiNike } from 'react-icons/si';
+>>>>>>> 790b196c723cce91e21cd5cf16861265cdbc5d36
 
 const Post = () => {
 	const [content, setContent] = useState({
 		user: undefined,
 		email: undefined,
-		title: undefined,
+		tech: undefined,
+		issue: undefined,
 		body: undefined,
+		rating: undefined,
 	});
 	const [submitted, setSubmitted] = useState(false);
 	const onChange = (e: any) => {
@@ -16,30 +23,52 @@ const Post = () => {
 		setContent((prevState) => ({ ...prevState, [name]: value }));
 	};
 	const onSubmit = async () => {
-		const { user, title, body }: any = content;
-		await axios.post('/api/entry', { user, title, slug: dashify(title), body });
+		const { user, email, tech, issue, body, rating }: any = content;
+		await axios.post('/api/entry', {
+			user,
+			tech,
+			email,
+			issue,
+			slug: dashify(user),
+			body,
+			rating,
+		});
 		setSubmitted(true);
 	};
 	return (
 		<div className="container">
-			<label htmlFor="title">Name</label>
+			<label htmlFor="user">Name</label>
 			<input type="text" name="user" value={content.user} onChange={onChange} />
-			<label htmlFor="title">Email</label>
+			<label htmlFor="tech">Tech</label>
+			<select name="tech" value={content.tech} onChange={onChange}>
+				<option value="topher">Topher</option>
+				<option value="tyler">Tyler</option>
+			</select>
+			<label htmlFor="email">Email</label>
 			<input
 				type="text"
 				name="email"
 				value={content.email}
 				onChange={onChange}
 			/>
-			<label htmlFor="title">Title</label>
+			<label htmlFor="title">Issue</label>
 			<input
 				type="text"
-				name="title"
-				value={content.title}
+				name="issue"
+				value={content.issue}
 				onChange={onChange}
 			/>
-			<label htmlFor="body">Body</label>
+			<label htmlFor="body">Detailed Description</label>
 			<textarea name="body" value={content.body} onChange={onChange} />
+
+			<label htmlFor="simple-controlled">Tech Rating</label>
+			<Rating
+				name="simple-controlled"
+				value={content.rating}
+				onChange={onChange}
+				icon={<SiNike fontSize="inherit" />}
+			/>
+
 			{!submitted ? (
 				<button onClick={onSubmit}>POST</button>
 			) : (
