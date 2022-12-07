eca4c90b07ef88b995ba8ef86a973f08cc31d562
Chris <ghost@drednaut.net>
2022-12-07 11:33:56 -0800

    updated posts page to filter based on tech
    Signed-off-by: Chris <ghost@drednaut.net>

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,30 +1,63 @@
 import Link from 'next/link';
 import db from '../../lib/db';
 import entries from '../api/entries';
+import { useState } from 'react';
 
 const Posts = (props: any) => {
 	const { entriesData } = props;
+	const [tech, setTech] = useState({
+		tech: undefined,
+	});
+
+	const onChange = (e: any) => {
+		const { value, name } = e.target;
+		setTech((prevState) => ({ ...prevState, [name]: value }));
+		console.log(tech.tech);
+	};
 
 	return (
 		<div className="container">
-			<h1>Posts: {entriesData.length}</h1>
-			{entriesData.map((entry: any) => (
-				<div key={entry.id} className="card">
-					<Link href={`/posts/${entry.slug}`}>
-						{entry.id}
-						<p>{new Date(entry.created).toLocaleString()}</p>
-					</Link>
-				</div>
-			))}
+			<h1>Total Submissions: {entriesData.length}</h1>
+			<label htmlFor="tyler">Tyler</label>
+			<input
+				type="radio"
+				name="tech"
+				value="tyler"
+				id="tyler"
+				onChange={onChange}
+			/>
+			<label htmlFor="topher">Topher</label>
+			<input
+				type="radio"
+				name="tech"
+				value="topher"
+				id="topher"
+				onChange={onChange}
+			/>
+			<label htmlFor="topher">Erik</label>
+			<input
+				type="radio"
+				name="tech"
+				value="erik"
+				id="erik"
+				onChange={onChange}
+			/>
+			{entriesData
+				.filter((entry: any) => entry.tech == tech.tech)
+				.map((entry: any) => (
+					<div key={entry.id} className="card">
+						<Link href={`/posts/${entry.slug}`}>
+							{entry.id}
+							<p>{new Date(entry.created).toLocaleString()}</p>
+						</Link>
+					</div>
+				))}
 		</div>
 	);
 };
 
 export const getStaticProps = async () => {
-	const entries = await db
-		.collection('incidents')
-		.where('tech', '==', 'tyler')
-		.get();
+	const entries = await db.collection('incidents').get();
 	const entriesData = entries.docs.map((entry) => ({
 		id: entry.id,
 		...entry.data(),
